e57df163a318eae573ce393380184e2073195451
anna_nikolaeva <nevaeh.burnside@yandex.ru>
2025-03-04 17:37:06 +0700

    Rectangle

--- FigureFabric.cs
+++ FigureFabric.cs
@@ -136,16 +136,19 @@ namespace GraphicEditor
         public Point PointOnCircle { get; }
         public PointF Center => center;
         public string Name => "Circle";
+
         public Circle(PointF center, PointF pointOnCircle)
         {
             Center = center;
             PointOnCircle = pointOnCircle;
         }
+
         public void Move(PointF vector)
         {
             center = new PointF(center.X + vector.X, center.Y + vector.Y);
             pointOnCircle = new PointF(pointOnCircle.X + vector.X, pointOnCircle.Y + vector.Y);
         }
+
          public void Rotate(PointF rotationCenter, float angle)
         {
             double rad = angle * Math.PI / 180;
@@ -170,6 +173,7 @@ namespace GraphicEditor
             center = new PointF(center.X * dx, center.Y * dy);
             pointOnCircle = new PointF(pointOnCircle.X * dx, pointOnCircle.Y * dy);
         }
+
         public void Scale(PointF scaleCenter, float dr)
         {
             center = new PointF(scaleCenter.X + (center.X - scaleCenter.X) * dr,
@@ -177,28 +181,34 @@ namespace GraphicEditor
             pointOnCircle = new PointF(scaleCenter.X + (pointOnCircle.X - scaleCenter.X) * dr,
                                        scaleCenter.Y + (pointOnCircle.Y - scaleCenter.Y) * dr);
         }
+
         public void Reflection(PointF a, PointF b)
         {
             center = ReflectPoint(center, a, b);
             pointOnCircle = ReflectPoint(pointOnCircle, a, b);
         }
+
         public IFigure Clone()
         {
             return new Circle(new PointF(center.X, center.Y), new PointF(pointOnCircle.X, pointOnCircle.Y));
         }
+
         public IEnumerable<IDrawingFigure> GetAsDrawable() => throw new NotImplementedException();
+
         public bool IsIn(PointF point, float eps)
         {
             float radius = Distance(center, pointOnCircle);
             float distToCenter = Distance(center, point);
             return Math.Abs(distToCenter - radius) <= eps;
         }
+
         private float Distance(PointF p1, PointF p2)
         {
             float dx = p1.X - p2.X;
             float dy = p1.Y - p2.Y;
             return (float)Math.Sqrt(dx * dx + dy * dy);
         }
+
         public IFigure Intersect(IFigure other) => throw new NotImplementedException();
         public IFigure Union(IFigure other) => throw new NotImplementedException();
         public IFigure Subtract(IFigure other) => throw new NotImplementedException();
@@ -210,3 +220,155 @@ namespace GraphicEditor
             throw new NotImplementedException();
     }
 }
+
+public class Rectangle : IFigure
+    {
+        private List<PointF> corners;
+
+        public Rectangle(PointF topLeft, float width, float height) // конструктор по левому верхнему углу, ширине и высоте
+        {
+            corners = new List<PointF>
+            {
+                topLeft,
+                new PointF(topLeft.X + width, topLeft.Y),
+                new PointF(topLeft.X + width, topLeft.Y + height),
+                new PointF(topLeft.X, topLeft.Y + height)
+            };
+        }
+
+        public Rectangle(List<PointF> corners) // конструктор по 4 точкам
+        {
+            if (corners == null || corners.Count != 4)
+                throw new ArgumentException("Для прямоугольника необходимо задать 4 вершины.");
+            this.corners = new List<PointF>(corners);
+        }
+
+        public string Name => "Rectangle";
+
+        public PointF Center
+        {
+            get
+            {
+                float sumX = 0, sumY = 0;
+                foreach (var pt in corners)
+                {
+                    sumX += pt.X;
+                    sumY += pt.Y;
+                }
+                return new PointF(sumX / corners.Count, sumY / corners.Count);
+            }
+        }
+
+        public void Move(PointF vector)
+        {
+            for (int i = 0; i < corners.Count; i++)
+            {
+                corners[i] = new PointF(corners[i].X + vector.X, corners[i].Y + vector.Y);
+            }
+        }
+
+        public void Rotate(PointF rotationCenter, float angle)
+        {
+            double rad = angle * Math.PI / 180;
+            double cosA = Math.Cos(rad);
+            double sinA = Math.Sin(rad);
+
+            for (int i = 0; i < corners.Count; i++)
+            {
+                corners[i] = RotatePoint(corners[i], rotationCenter, cosA, sinA);
+            }
+        }
+
+        private PointF RotatePoint(PointF pt, PointF center, double cosA, double sinA)
+        {
+            float dx = pt.X - center.X;
+            float dy = pt.Y - center.Y;
+            return new PointF(
+                (float)(center.X + dx * cosA - dy * sinA),
+                (float)(center.Y + dx * sinA + dy * cosA)
+            );
+        }
+
+        public void Scale(float dx, float dy)
+        {
+            for (int i = 0; i < corners.Count; i++)
+            {
+                corners[i] = new PointF(corners[i].X * dx, corners[i].Y * dy);
+            }
+        }
+
+        public void Scale(PointF scaleCenter, float dr)
+        {
+            for (int i = 0; i < corners.Count; i++)
+            {
+                float newX = scaleCenter.X + (corners[i].X - scaleCenter.X) * dr;
+                float newY = scaleCenter.Y + (corners[i].Y - scaleCenter.Y) * dr;
+                corners[i] = new PointF(newX, newY);
+            }
+        }
+
+        public void Reflection(PointF a, PointF b)
+        {
+            for (int i = 0; i < corners.Count; i++)
+            {
+                corners[i] = ReflectPoint(corners[i], a, b);
+            }
+        }
+
+        private PointF ReflectPoint(PointF p, PointF a, PointF b)
+        {
+            float dx = b.X - a.X;
+            float dy = b.Y - a.Y;
+            float mag2 = dx * dx + dy * dy;
+            if (mag2 == 0) return p;
+            float t = ((p.X - a.X) * dx + (p.Y - a.Y) * dy) / mag2;
+            PointF proj = new PointF(a.X + t * dx, a.Y + t * dy);
+            return new PointF(2 * proj.X - p.X, 2 * proj.Y - p.Y);
+        }
+
+        public IFigure Clone()
+        {
+            return new Rectangle(new List<PointF>(corners));
+        }
+
+        public void Draw(IDrawing drawing) => throw new NotImplementedException();
+
+        // Проверка, находится ли точка внутри прямоугольника.
+        // Реализовано с помощью алгоритма «лучевого пересечения».
+        public bool IsIn(PointF point, float eps)
+        {
+            bool inside = false;
+            int j = corners.Count - 1;
+            for (int i = 0; i < corners.Count; i++)
+            {
+                if (((corners[i].Y > point.Y) != (corners[j].Y > point.Y)) &&
+                    (point.X < (corners[j].X - corners[i].X) * (point.Y - corners[i].Y) / (corners[j].Y - corners[i].Y) + corners[i].X))
+                    inside = !inside;
+                j = i;
+            }
+            return inside;
+        }
+
+        public IFigure Intersect(IFigure other) => throw new NotImplementedException();
+        public IFigure Union(IFigure other) => throw new NotImplementedException();
+        public IFigure Subtract(IFigure other) => throw new NotImplementedException();
+
+        public void SetParameters(IDictionary<string, double> doubleParams, IDictionary<string, PointF> pointParams) // это так делается???? 
+        {
+            if (pointParams != null &&
+                pointParams.ContainsKey("TopLeft") &&
+                pointParams.ContainsKey("TopRight") &&
+                pointParams.ContainsKey("BottomRight") &&
+                pointParams.ContainsKey("BottomLeft"))
+            {
+                corners[0] = pointParams["TopLeft"];
+                corners[1] = pointParams["TopRight"];
+                corners[2] = pointParams["BottomRight"];
+                corners[3] = pointParams["BottomLeft"];
+            }
+            else
+            {
+                throw new ArgumentException("Для Rectangle необходимы параметры 'TopLeft', 'TopRight', 'BottomRight' и 'BottomLeft'.");
+            }
+        }
+    }
